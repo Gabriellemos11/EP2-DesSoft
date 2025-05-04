5b6d756cec9a2a3869fd6081d2270479d8b731f1
Pedrocam19 <pedrocam@al.insper.edu.br>
2025-05-04 14:45:59 -0300

    questao 1

--- funcoes.py
+++ funcoes.py
@@ -5,6 +5,3 @@ def rolar_dados(qtd_dados):
     for _ in range(qtd_dados):
         resultado.append(random.randint(1, 6))
     return resultado
-
-if __name__ == "__main__":
-    print(rolar_dados(5))
