ca08c1e3b450d346dc5a870bdef5aeeb93c6e71e
Pedrocam19 <pedrocam@al.insper.edu.br>
2025-05-04 15:02:46 -0300

    agora

--- funcoes.py
+++ funcoes.py
@@ -7,6 +7,7 @@ def rolar_dados(qtd_dados):
     return resultado
 
 def guardar_dado(dados_rolados, dados_no_estoque, dado_para_guardar):
-    dados_no_estoque.append(dado_para_guardar)
-    dados_rolados.remove(dado_para_guardar)
+    dado = dados_rolados[dado_para_guardar]
+    dados_no_estoque = dados_no_estoque.append(dado)
+    dados_rolados.pop(dado_para_guardar)
     return [dados_rolados, dados_no_estoque]
