e908490f0c1e9f9571ca34ae632261267477d0f7
Pedrocam19 <pedrocam@al.insper.edu.br>
2025-05-04 15:01:09 -0300

    2

--- funcoes.py
+++ funcoes.py
@@ -5,3 +5,8 @@ def rolar_dados(qtd_dados):
     for _ in range(qtd_dados):
         resultado.append(random.randint(1, 6))
     return resultado
+
+def guardar_dado(dados_rolados, dados_no_estoque, dado_para_guardar):
+    dados_no_estoque.append(dado_para_guardar)
+    dados_rolados.remove(dado_para_guardar)
+    return [dados_rolados, dados_no_estoque]
