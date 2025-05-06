945314eef91ede3c8a584d7dd9871a34eaf78af4
Pedrocam19 <pedrocam@al.insper.edu.br>
2025-05-06 18:48:53 -0300

    13(12)

--- programa.py
+++ programa.py
@@ -1,43 +1,38 @@
-from funcoes import rolar_dados, guardar_dado, remover_dado, calcula_pontuacao
-
-if __name__ == "__main__":
-    cartela = {
-        "1": None, "2": None, "3": None, "4": None, "5": None, "6": None,
-        "sem_combinacao": None, "quadra": None, "full_house": None,
-        "sequencia_baixa": None, "sequencia_alta": None, "cinco_iguais": None
-    }
-
-    dados_guardados = []
-    dados_rolados = rolar_dados(5)
-    print("Cartela de Pontos:")
-    print("-------------------------")
-    for k in cartela:
-        print(f"| {k:<15} | {cartela[k] if cartela[k] is not None else '':>2} |")
-    print("-------------------------")
-    print(f"Dados rolados: {dados_rolados}")
-    print(f"Dados guardados: {dados_guardados}")
+    print("Digite 1 para guardar um dado, 2 para remover um dado, 3 para rerrolar, 4 para ver a cartela ou 0 para finalizar a rodada.")
 
     while True:
-        print("Digite 1 para guardar um dado, 2 para remover um dado, 3 para encerrar a rodada")
-        comando = input("Escolha: ")
-        if comando == "1":
-            pos = int(input("Qual a posição do dado que deseja guardar? (0 a n): "))
-            dados_rolados, dados_guardados = guardar_dado(dados_rolados, dados_guardados, pos)
-        elif comando == "2":
-            pos = int(input("Qual a posição do dado que deseja remover? (0 a n): "))
-            dados_rolados, dados_guardados = remover_dado(dados_rolados, dados_guardados, pos)
-        elif comando == "3":
-            break
-        print(f"Dados rolados: {dados_rolados}")
-        print(f"Dados guardados: {dados_guardados}")
+        opcao = input('> ')
+        
+        if opcao == '1':
+            print("Digite o índice do dado a ser guardado (0 a 4):")
+            indice = int(input('> '))
+            dados, dados_no_estoque = guardar_dado(dados, dados_no_estoque, indice)
+            print('Dados rolados: {}'.format(dados))
+            print('Dados guardados: {}'.format(dados_no_estoque))
 
-    dados_finais = dados_guardados + dados_rolados
-    categoria = input("Digite uma categoria: ")
-    pontuacao = calcula_pontuacao(dados_finais, categoria)
-    cartela[categoria] = pontuacao
+        elif opcao == '2':
+            print("Digite o índice do dado a ser removido (0 a 4):")
+            indice = int(input('> '))
+            dados, dados_no_estoque = remover_dado(dados, dados_no_estoque, indice)
+            print('Dados rolados: {}'.format(dados))
+            print('Dados guardados: {}'.format(dados_no_estoque))
 
-    print("Cartela de Pontos:")
-    print("-------------------------")
-    for k in cartela:
-        print(f"| {k:<15} | {cartela[k] if cartela[k] is not None else '':>2} |")
-    print("-------------------------")
+        elif opcao == '3':
+            dados = rerrolar(dados, 5 - len(dados_no_estoque))
+            print('Dados rerrolados: {}'.format(dados))
+
+        elif opcao == '4':
+            imprime_cartela(cartela)
+
+        elif opcao == '0':
+            dados = dados_no_estoque + dados  # juntar todos os dados
+            combinacao = faz_jogada(dados)
+            combinacoes.append(combinacao)
+            cartela, pontos = pontuar(cartela, combinacao)
+            print('Combinação escolhida:', combinacao)
+            print('Pontuação da jogada:', pontos)
+            rodada += 1
+            break  # sai do while interno e volta para a próxima rodada
+
+        else:
+            print("Opção inválida. Tente novamente.")
